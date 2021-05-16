12d3005103df3af43bec58e381108a91979e3b87
anoduck <anoduck@users.noreply.github.com>
2021-05-16 02:11:00 -0400

    Removed old pointless code

--- scraper/photo-scrape.py
+++ scraper/photo-scrape.py
@@ -545,66 +545,7 @@ def scroll():
 
     return
 
-# ****************************************************************************
-# *                                   Utils                                  *
-# ****************************************************************************
-# The following functions are deprecated
 
-
-def get_title_links(title):
-    l = title.find_elements_by_tag_name("a")  # noqa: E741
-    return l[-1].text, l[-1].get_attribute("href")
-
-
-def get_title(x, selectors):
-    title = ""
-    try:
-        title = x.find_element_by_xpath(selectors.get("title"))
-    except Exception:
-        try:
-            title = x.find_element_by_xpath(selectors.get("title_exc1"))
-        except Exception:
-            try:
-                title = x.find_element_by_xpath(selectors.get("title_exc2"))
-            except Exception:
-                pass
-    finally:
-        return title
-
-
-def get_time(x):
-    time = ""
-    try:
-        time = x.find_element_by_tag_name("abbr").get_attribute("title")
-        time = (
-            str("%02d" % int(time.split(", ")[1].split()[1]),)
-            + "-"
-            + str(
-                (
-                    "%02d"
-                    % (
-                        int(
-                            (
-                                list(calendar.month_abbr).index(
-                                    time.split(", ")[1].split()[0][:3]
-                                )
-                            )
-                        ),
-                    )
-                )
-            )
-            + "-"
-            + time.split()[3]
-            + " "
-            + str("%02d" % int(time.split()[5].split(":")[0]))
-            + ":"
-            + str(time.split()[5].split(":")[1])
-        )
-    except Exception:
-        pass
-
-    finally:
-        return time
 
 # ## Defining the scraping process
 
